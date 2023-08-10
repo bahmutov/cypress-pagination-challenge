a0135839f58f915e16806314849098c6866090b3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-08-10 08:42:06 -0400

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# cypress-pagination-challenge
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,18 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+
+beforeEach(() => {
+  cy.visit('public/index.html')
+})
+
+it('clicks the Next button until we get to the last page', () => {
+  // the HTML table on the page is paginated
+  // can you click the "Next" button until
+  // we get to the very last page?
+  // button selector "[value=next]"
+  cy.get('[value=next]') // keep clicking!
+  // confirm we are on the very last page of the table
+  cy.get('[value=next]').should('be.disabled')
+  cy.get('[value=last]').should('be.disabled')
+})
