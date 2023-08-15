25a6c5497b6b5597e0150f586ab2a4ac02497eda
github-personal <paf_uzair@yahoo.com>
2023-08-16 00:25:43 +0200

    chore: added solution using recursive approach

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,18 +2,38 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 
+const nextBtn = '[value=next]'
+const lastBtn = '[value=last]'
+
 beforeEach(() => {
   cy.visit('public/index.html')
 })
 
-it('clicks the Next button until we get to the last page', () => {
+it('clicks the Next button until we get to the last page', async () => {
   // the HTML table on the page is paginated
   // can you click the "Next" button until
   // we get to the very last page?
   // button selector "[value=next]"
-  cy.get('[value=next]') // keep clicking!
+
+  cy.get(nextBtn)
+    .should('exist')
+    .then($button => {
+      function clickNext () {
+        cy.get(nextBtn).then($button => {
+          if ($button.attr('disabled')) {
+            cy.log('Button is disabled, pagination finished!')
+          } else {
+            cy.log('Clicking the "Next" button...')
+            cy.get(nextBtn).click().should('exist')
+            clickNext()
+          }
+        })
+      }
+
+      clickNext()
+    })
 
   cy.log('**confirm we are on the last page**')
-  cy.get('[value=next]').should('be.disabled')
-  cy.get('[value=last]').should('be.disabled')
+  cy.get(nextBtn).should('be.disabled')
+  cy.get(lastBtn).should('be.disabled')
 })
